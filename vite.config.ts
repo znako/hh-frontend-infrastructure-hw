770cc8dd9a0dad5b751f5b975694038277fe62f3
Знай Артемий <artem.znay@mail.ru>
2023-11-21 23:14:15 +0300

    Add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '',
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
